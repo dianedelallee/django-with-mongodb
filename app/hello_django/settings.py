36d86f071a196661a1668d8e3702f861135d4255
Diane Delallée <diane.delallee@pix4d.com>
2020-01-17 11:22:40 +0100

    update database configuration in settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "ENGINE": os.environ.get("NOSQL_ENGINE", "djongo"),
+        "NAME": os.environ.get("NOSQL_DATABASE", os.path.join(BASE_DIR, "myapp-djongo-db")),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
-    }
+    },
 }
 
 
